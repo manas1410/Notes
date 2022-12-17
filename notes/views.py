965d5189c02642746600dec3184f803b75448983
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-12-18 00:27:08 +0530

    Foreign Key

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.http.response import HttpResponseRedirect
 from django.views.generic import UpdateView, CreateView, DetailView,ListView
 from django.views.generic.edit  import DeleteView 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Notes 
 from .forms import NotesForm
@@ -16,15 +18,25 @@ class NotesUpdateView(UpdateView):
     form_class = NotesForm
     success_url = '/smart/notes'
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin ,CreateView):
     model = Notes
     form_class = NotesForm
     success_url = '/smart/notes'
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit= False)
+        self.object.user = self.request.user 
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 class NotesListView(ListView):
     model = Notes 
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes 
